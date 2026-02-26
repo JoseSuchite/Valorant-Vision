d726c0128dad1abdd81b1c1bcfe0670d6eecec98
KiingdomKey <allthegoodemailshavebeentaken1@gmail.com>
2026-02-26 09:29:20 -0600

    Restructured and added pause/play function

--- cpp/videoplayer.cpp
+++ cpp/videoplayer.cpp
@@ -9,25 +9,36 @@
 #include "../headers/videoplayer.h"
 
 // Takes in a pointer to the parent object
-VideoPlayer::VideoPlayer(QWidget *parentAddress) {
-	this->parentPointer = parentAddress;
+VideoPlayer::VideoPlayer(QWidget *parentAddress) : 
+	QWidget(parentAddress)
+{
+	player = new QMediaPlayer(this);
+	videoWidget = new QVideoWidget(this);
+	audioOutput = new QAudioOutput(this);
+
+	player->setVideoOutput(videoWidget);
+	player->setAudioOutput(audioOutput);
+
+	QVBoxLayout *layout = new QVBoxLayout(this);
+	layout->addWidget(videoWidget);
+
 }
 
-//Takes in the url to a video and displays its
+//Takes in the url to a video and displays it
 void VideoPlayer::displayVideo(std::string source) {
 
 	//Convert our string into a QString (which Qt can use)
 	QString QSource = QString::fromStdString(source);
-
-	QMediaPlayer *player = new QMediaPlayer(this->parentPointer);
 	player->setSource(QUrl::fromLocalFile(QSource));
 
-	QVideoWidget *videoWidget = new QVideoWidget(this->parentPointer);
-	player->setVideoOutput(videoWidget);
-
-	QAudioOutput *audioOutput = new QAudioOutput(this->parentPointer);
-	player->setAudioOutput(audioOutput);
-
-	videoWidget->show();
 	player->play();
+}
+
+void VideoPlayer::pauseOrPlayVideo() {
+	if (player->playbackState() == QMediaPlayer::PlaybackState::PausedState) {
+		player->play();
+	}
+	else {
+		player->pause();
+	}
 }
