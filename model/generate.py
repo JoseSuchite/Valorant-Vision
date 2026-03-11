9dabf58ddcba266239932910b157d4bedf41d916
KiingdomKey <allthegoodemailshavebeentaken1@gmail.com>
2026-03-11 15:08:25 -0500

    Updated

--- model/generate.py
+++ model/generate.py
@@ -5,9 +5,10 @@ if __name__ == "__main__":
     current_dir = os.getcwd()
     generator = ImageGenerator(agent_icon_path=current_dir+"/agent_icons", 
                             minimap_path=current_dir+"/map_layouts",
-                            agent_resize_factor=(32, 32))
+                            misc_icon_path=current_dir+"/misc_icons",
+                            agent_resize_factor=(64, 64))
 
-    for i in range(128):
+    for i in range(4):
         data_name = f"{i:04d}"
         generator.generate_data(data_name, is_train=True)
         generator.generate_data(data_name, is_train=False)
