8d4d67ccc585781b1ec59abab4efc03bfcaaed40
rednose14 <krishdesai72@gmail.com>
2026-03-14 00:25:52 -0500

    added the minimap and required files

---
+++ headers/minimap.h
@@ -0,0 +1,26 @@
+#ifndef MINIMAP_H
+#define MINIMAP_H
+
+#include <QDockWidget>
+#include <QLabel>
+#include <QPixmap>
+
+// This class is used by the side bar to show logs and reset bar
+class Minimap : public QDockWidget {
+
+private:
+	QLabel* minimap_wid;
+	QPixmap pixmap;
+
+public:
+	// sets the parent to nullptr if no main parent
+	explicit Minimap(QWidget* parent = nullptr);
+
+	// allows to load an image to the minimap widget
+	void loadImage(const QString &file);
+
+	// allows the image in the widget to resize without losing the quality
+	void resizeEvent(QResizeEvent *event);
+};
+
+#endif
