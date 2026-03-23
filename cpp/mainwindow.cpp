b398b689d59f30990c41a7f807308f313c3c322f
RogueSlicer <hbrownwork29@gmail.com>
2026-03-23 17:25:01 -0500

    Began editing the UI. Added a control panel with pause/play button and volume slider.

--- cpp/mainwindow.cpp
+++ cpp/mainwindow.cpp
@@ -3,10 +3,13 @@
 #include <QWidget>
 #include <QMainWindow>
 #include <QVBoxLayout>
+#include <QHBoxLayout>
 #include <QPushButton>
 #include <QFileDialog>
 #include <QObject>
 #include <QFileInfo>
+#include <QPixmap>
+#include <QLabel>
 
 #include "../headers/logbar.h"
 #include "../headers/mainwindow.h"
@@ -24,22 +27,43 @@ MainWindow::MainWindow() :
         addDockWidget(Qt::RightDockWidgetArea, logBar);
         player = new VideoPlayer(centralWidget);
         mainVerticalLayout = new QVBoxLayout(centralWidget);
+        controlPanel = new QHBoxLayout(centralWidget);
         chooseFileButton = new QPushButton("Select Video", centralWidget);
+
+        QPixmap pixmap(":/images/logo.png");
+        picLabel = new QLabel();
+        picLabel->setPixmap(pixmap);
+
         pauseButton = new QPushButton("Pause/Play", centralWidget);
+        pauseButton->setStyleSheet(QString("background-color: red;"));
+
+        volumeSlider = new QSlider(Qt::Horizontal, this);
+        volumeSlider->setRange(0, 100);
+        volumeSlider->setValue(100);
+
         logButton = new QPushButton("Reset Log Bar", centralWidget);
 
         QObject::connect(chooseFileButton, &QPushButton::clicked, this, &MainWindow::openAndPlayVideoOnClick);
         QObject::connect(pauseButton, &QPushButton::clicked, this, &MainWindow::pauseOrPlayVideo);
         QObject::connect(logButton, &QPushButton::clicked, this, &MainWindow::resetBar);
 
+        QObject::connect(volumeSlider, &QSlider::valueChanged, this, [this](int value) {player->setVolume(value / 100.0f);});
+
         this->setCentralWidget(centralWidget);
 
+        controlPanel->addWidget(pauseButton);
+        controlPanel->addWidget(volumeSlider);
+
+        mainVerticalLayout->addWidget(picLabel);
         mainVerticalLayout->addWidget(player);
         mainVerticalLayout->addWidget(chooseFileButton);
-        mainVerticalLayout->addWidget(pauseButton);
+        mainVerticalLayout->addLayout(controlPanel);
         mainVerticalLayout->addWidget(logButton);
 
-
+        volumeSlider->hide();
+        pauseButton->hide();
+        logBar->hide();
+        logButton->hide();
         player->hide();
         this->show();
 }
@@ -56,6 +80,12 @@ void MainWindow::openAndPlayVideoOnClick() {
 
     std::string source = QSource.toStdString();
 
+    volumeSlider->show();
+    chooseFileButton->hide();
+    picLabel->hide();
+    pauseButton->show();
+    logBar->show();
+    logButton->show();
     player->show();
     player->displayVideo(source);
     logBar->addLog("Video Loaded.");
