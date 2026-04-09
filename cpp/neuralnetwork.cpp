c690159cb2c25b7a7678f8759207ac1d16746789
KiingdomKey <allthegoodemailshavebeentaken1@gmail.com>
2026-04-09 09:54:46 -0500

    Updated to use BYTETracker tracking

--- cpp/neuralnetwork.cpp
+++ cpp/neuralnetwork.cpp
@@ -13,17 +13,16 @@ NeuralNetwork::NeuralNetwork(const T_CHAR *onnxFilePath, const int ROWS, const i
     this->COLS = COLS;
 
     //Set up environment and session options
-	env = new Ort::Env(ORT_LOGGING_LEVEL_WARNING, "Valorant-Vision Model");
-	sessionOptions.SetIntraOpNumThreads(1);
-	sessionOptions.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_ALL);
+    env = new Ort::Env(ORT_LOGGING_LEVEL_WARNING, "Valorant-Vision Model");
+    sessionOptions.SetIntraOpNumThreads(1);
+    sessionOptions.SetGraphOptimizationLevel(GraphOptimizationLevel::ORT_ENABLE_ALL);
 
     //Create session
     try {
         model = new Ort::Session(*env, onnxFilePath, sessionOptions);
     }
     catch (Ort::Exception &e) {
-        std::cerr << "ONNX Runtime error while creating model: " << e.what() << std::endl;
-        std::exit(-1);
+        throw Ort::Exception("ONNX Runtime error while creating model: " + e.what());
     }
 
     //Get and store input names for the model
@@ -47,6 +46,15 @@ NeuralNetwork::NeuralNetwork(const T_CHAR *onnxFilePath, const int ROWS, const i
     for (auto &name : outputNodeNames) {
         outputNodeNamesCStyle.push_back(name.c_str());
     }
+    //std::string reidWeightsName = "E:/My Program Files/git/repos/Valorant-Vision_old/build/_deps/motcpp-src/scripts/models/osnet_x1_0_dukemtmcreid.onnx";
+
+    tracker = new motcpp::trackers::ByteTrack(false,
+        false,
+        0.4f,
+        30,
+        50,
+        3,
+        0.8f);
 }
 
 NeuralNetwork::~NeuralNetwork() {
@@ -54,8 +62,19 @@ NeuralNetwork::~NeuralNetwork() {
 	model = nullptr;
 	delete env;
 	env = nullptr;
+    delete tracker;
+    tracker = nullptr;
 }
 
+Eigen::MatrixXf NeuralNetwork::getCurrentPrediction() const {
+    return currentPrediction;
+}
+
+Eigen::MatrixXf NeuralNetwork::getCurrentTracking() const {
+    return currentTracking;
+}
+
+//Takes in an image, standardizes it, and returns it as an array of pixel values
 std::vector<float> NeuralNetwork::prepareImage(const cv::Mat &imageBGR) {
     cv::Mat imageBlob = cv::dnn::blobFromImage(imageBGR,
         1.0 / 255.0, //Standardize pixel values
@@ -71,7 +90,7 @@ std::vector<float> NeuralNetwork::prepareImage(const cv::Mat &imageBGR) {
     return imageVector;
 }
 
-Prediction NeuralNetwork::predict(cv::Mat imageBGR) {
+void NeuralNetwork::predict(cv::Mat imageBGR) {
 
     size_t input_tensor_size = 1 * 3 * ROWS * COLS;
 
@@ -115,47 +134,66 @@ Prediction NeuralNetwork::predict(cv::Mat imageBGR) {
     float *scoresArr = outputTensor[2].GetTensorMutableData<float>();
     size_t scoresLength = outputTensor[2].GetTensorTypeAndShapeInfo().GetElementCount();
 
-    Prediction prediction;
-    prediction.length = 0;
-    for (int i = 0; i < labelsLength; i++) { //Length of labels and scores *should* be equal, so I just arbitrarily picked one
-        prediction.length++;
-
-        //Each bounding box is stored as 4 points, so add them
-        Box box;
-        box.xmin = boxesArr[4 * i];
-        box.ymin = boxesArr[4 * i + 1];
-        box.xmax = boxesArr[4 * i + 2];
-        box.ymax = boxesArr[4 * i + 3];
-        prediction.boundingBoxes.push_back(box);
-
-        //Add the label and score
-        prediction.labels.push_back(labelsArr[i]);
-        prediction.scores.push_back(scoresArr[i]);
+    currentPrediction = Eigen::MatrixXf(labelsLength, 6);
+    for (int i = 0; i < labelsLength; i++) {
+        currentPrediction(i, 0) = boxesArr[4 * i];
+        currentPrediction(i, 1) = boxesArr[4 * i + 1];
+        currentPrediction(i, 2) = boxesArr[4 * i + 2];
+        currentPrediction(i, 3) = boxesArr[4 * i + 3];
+        currentPrediction(i, 4) = scoresArr[i];
+        currentPrediction(i, 5) = labelsArr[i];
     }
 
-    return prediction;
-
+    currentTracking = tracker->update(currentPrediction, imageBGR);
 }
 
-void NeuralNetwork::outputImageWithBoxesAndLabels(const cv::Mat image, const Prediction prediction, const float threshold) {
-    
+
+void NeuralNetwork::outputClassificationImageWithBoxesAndLabels(const cv::Mat image, const Eigen::MatrixXf prediction, const float threshold, std::string outFileName) {
+
     //Resize the image to what's used internally
     cv::Mat resizedImg;
     cv::resize(image, resizedImg, cv::Size(COLS, ROWS));
 
-    for (int i = 0; i < prediction.length; i++) {
-        if (prediction.scores[i] < threshold) { continue; }
+    for (int i = 0; i < prediction.rows(); i++) {
+        if (prediction(i, 4) < threshold) { continue; }
 
-        Box box = prediction.boundingBoxes[i];
+        int xmin = prediction(i, 0);
+        int ymin = prediction(i, 1);
+        int xmax = prediction(i, 2);
+        int ymax = prediction(i, 3);
 
-        cv::String text = std::to_string(prediction.labels[i]) + ": " + std::to_string((int) (prediction.scores[i] * 100)) + "%";
+        cv::String text = std::to_string(prediction(i, 5)) + ": " + std::to_string((int)(prediction(i, 4) * 100)) + "%";
 
         //Put label text on screen (the first one is to give an outline to the text (it is really hard to read otherwise))
-        cv::putText(resizedImg, text, cv::Point(box.xmin, box.ymin - 10), cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(0, 0, 0), 3, cv::LINE_AA);
-        cv::putText(resizedImg, text, cv::Point(box.xmin, box.ymin - 10), cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(0, 255, 0), 2, cv::LINE_AA);
+        cv::putText(resizedImg, text, cv::Point(xmin, ymin - 10), cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(0, 0, 0), 3, cv::LINE_AA);
+        cv::putText(resizedImg, text, cv::Point(xmin, ymin - 10), cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(0, 255, 0), 2, cv::LINE_AA);
 
-        cv::rectangle(resizedImg, cv::Rect(box.xmin, box.ymin, box.xmax - box.xmin, box.ymax - box.ymin), cv::Scalar(0, 0, 255), 2);
+        cv::rectangle(resizedImg, cv::Rect(xmin, ymin, xmax - xmin, ymax - ymin), cv::Scalar(0, 0, 255), 2);
 
     }
-    cv::imwrite("image_labeled.png", resizedImg);
+    cv::imwrite(outFileName, resizedImg);
+}
+
+void NeuralNetwork::outputTrackingImageWithBoxesAndLabels(const cv::Mat image, Eigen::MatrixXf tracks, std::string outFileName) {
+    //Resize the image to what's used internally
+    cv::Mat resizedImg;
+    cv::resize(image, resizedImg, cv::Size(COLS, ROWS));
+
+    for (int i = 0; i < tracks.rows(); i++) {
+
+        int xmin = tracks(i, 0);
+        int ymin = tracks(i, 1);
+        int xmax = tracks(i, 2);
+        int ymax = tracks(i, 3);
+
+        cv::String text = "#" + std::to_string((int)tracks(i, 4)) + ": " + std::to_string((int)tracks(i, 6));
+
+        //Put label text on screen (the first one is to give an outline to the text (it is really hard to read otherwise))
+        cv::putText(resizedImg, text, cv::Point(xmin, ymin - 10), cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(0, 0, 0), 3, cv::LINE_AA);
+        cv::putText(resizedImg, text, cv::Point(xmin, ymin - 10), cv::FONT_HERSHEY_SIMPLEX, 0.4, cv::Scalar(0, 255, 0), 2, cv::LINE_AA);
+
+        cv::rectangle(resizedImg, cv::Rect(xmin, ymin, xmax - xmin, ymax - ymin), cv::Scalar(0, 0, 255), 2);
+
+    }
+    cv::imwrite(outFileName, resizedImg);
 }
--- headers/neuralnetwork.h
+++ headers/neuralnetwork.h
@@ -2,10 +2,20 @@
 #define NEURALNETWORK_H
 
 #include <filesystem>
+#include <unordered_map>
 #include <string>
 
 #include <onnxruntime_cxx_api.h>
 #include <opencv2/opencv.hpp>
+
+#include "Eigen/Dense"
+
+#include <motcpp/trackers/bytetrack.hpp>
+#include <motcpp/trackers/deepocsort.hpp>
+#include <motcpp/trackers/strongsort.hpp>
+#include <motcpp/trackers/oracletrack.hpp>
+#include <motcpp/trackers/boosttrack.hpp>
+
 
 //Make sure we use the right type of char, because ONNX on Windows has to be unique and use wchar_t...
 //Example usage: const T_CHAR *str = T_TEXT("Chikorita is the best pokemon.")
@@ -17,31 +27,17 @@ typedef char T_CHAR;
 #define T_TEXT(quote) quote
 #endif
 
-// Struct for the coordinates of a prediction box
-struct Box {
-	int xmin = 0;
-	int ymin = 0;
-	int xmax = 0;
-	int ymax = 0;
-};
-
-// struct for holding the results of a prediction
-struct Prediction {
-	size_t length = 0;
-	std::vector<Box> boundingBoxes;
-	std::vector<int64_t> labels;
-	std::vector<float> scores;
-};
-
 class NeuralNetwork {
 
 private:
 
-	Ort::Env *env;
+	Ort::Env *env = nullptr;
 	Ort::SessionOptions sessionOptions;
-	Ort::Session *model; // Has to be dynamically created because we can't create it with no parameters
+	Ort::Session *model = nullptr; // Has to be dynamically created because we can't create it with no parameters
 	int ROWS; //Height of images to use
 	int COLS; //Width of images to use
+
+	motcpp::trackers::ByteTrack *tracker = nullptr; //Tracker object
 
 	//Storing an std::string version and a c-style version separaretly
 	//We need c-style to pass into the model but that gets really annoying and yes I know this is ineffecient but like, it's like 4 extra strings
@@ -52,19 +48,30 @@ private:
 	std::vector<std::string> outputNodeNames;
 	std::vector<const char *> outputNodeNamesCStyle;
 
+	Eigen::MatrixXf currentPrediction; //Stores current raw prediction info from model in format: x1, y1, x2, y2, confidence, classID
+	Eigen::MatrixXf currentTracking; //Stores current tracking info in format: x1, y1, x2, y2, trackID, confidence, classID, DetIndex
+
+
 	//Takes in a reference to an opencv image file, does needed preprocessing, and returns it as a vector of floats
 	std::vector<float> prepareImage(const cv::Mat &);
+
 
 public:
 
 	NeuralNetwork(const T_CHAR *onnxFilePath, const int ROWS, const int COLS);
 	~NeuralNetwork();
 
-	//Takes in an image in opencv BGR format and returns prediction on it
-	Prediction predict(cv::Mat imageBGR);
+	Eigen::MatrixXf getCurrentPrediction() const;
+	Eigen::MatrixXf getCurrentTracking() const;
+
+	//Takes in an image in opencv BGR format and updates internal prediction
+	void predict(cv::Mat imageBGR);
 
 	//Outputs an image file with predictions drawn on it. Takes in the image, arrays of bounding boxes, labels, and scores, the amount of predictions (N), and a threshold for scores to show up
-	void outputImageWithBoxesAndLabels(const cv::Mat image, const Prediction prediction, const float threshold);
+	void outputClassificationImageWithBoxesAndLabels(const cv::Mat image, const Eigen::MatrixXf prediction, const float threshold, std::string outFileName);
+
+	//Outputs an image file with tracking information drawn on it. Takes in the image, eigen matrix of tracking information, and the name of the file to be created
+	void outputTrackingImageWithBoxesAndLabels(const cv::Mat image, Eigen::MatrixXf, std::string outFileName);
 };
 
 #endif
